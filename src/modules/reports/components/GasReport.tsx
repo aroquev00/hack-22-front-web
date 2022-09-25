101e222fab711e57ff0e3bf0c15447c6c2017811
aroquev00 <aroquev00@users.noreply.github.com>
2022-09-25 08:54:03 -0500

    Upload repo

--- src/modules/reports/components/GasReport.tsx
+++ src/modules/reports/components/GasReport.tsx
@@ -1,11 +1,28 @@
 import { Container, Typography } from "@mui/material";
-import React from "react";
+import { useEffect, useState } from "react";
+import Table from "./Table";
+import User from "../../users/interfaces/User";
+import {getUserData} from "../../general/utils/requests";
+import {URLs} from "../../general/utils/URLs";
 
-import Table from "./Table"
+
 export default function RegistrarCliente() {
+
+    const [user, setUser] = useState<User | undefined>(undefined);
+
+    useEffect(() => {
+
+        fetch(URLs.users + "/user1")
+            .then(response =>
+                response.json()).then(data => {
+            setUser(data);
+        });
+    }, []);
+
     return(
         <Container maxWidth="lg" style={{display: 'flex', alignItems: 'center', flexDirection: 'column'}}>
             <Typography variant="h3" style={{margin: '10px'}}> Gas Report </Typography>
+            <Typography variant="h5" style={{margin: '10px'}}> Hi {user?.name} </Typography>
             <Table />
         </Container>
     );
